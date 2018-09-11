dcf09956ee4511c95840f43254b8d6b3b9994657
Kwaku <boafo819@gmail.com>
2018-09-10 21:12:41 -0400

    A post-it theme productivity app

---
+++ script.js
@@ -0,0 +1,27 @@
+// clicking the create Post button 
+
+let postButton = document.getElementById("createPostIcon");
+
+function postEvent(){
+    
+    let link = document.getElementById("link");
+    link.href = "post.html";
+}
+
+postButton.addEventListener("click" , postEvent);
+
+// set database object 
+const database = firebase.database().ref();
+
+database.on("child_added",addMessage);
+
+function addMessage(rowData){
+   const row = rowData.val();
+   const name = row.USERNAME;
+   const msg = row.MESSAGE;
+   let messageDiv = document.getElementById("posts");
+   let para = document.createElement("p");
+   para.className = "postIt2";
+   para.innerText = `${name}: ${msg}`;
+   messageDiv.appendChild(para)
+}
---
+++ script2.js
@@ -0,0 +1,43 @@
+const usernameElement = document.getElementById("username");
+const messageElement = document.getElementById("message");
+const button = document.getElementById("submitButton");
+button.addEventListener("click", updateDB);
+
+//Set database object here |firebase.database().ref|
+
+const database =  firebase.database().ref();
+/**
+ * Updates the database with the username and message.
+ */
+function updateDB(event){
+    
+    let link2 = document.getElementById("link2");
+    link2.href = "index.html";
+    const username        = usernameElement.value;
+    const message         = messageElement.value;
+
+    usernameElement.value = "";
+    messageElement.value  = "";
+
+    console.log(username + " : " + message);
+
+    //Update database here
+    if(username != "" || message != ""){
+    const data = {
+        USERNAME: username,
+        MESSAGE: message
+    }
+    database.push(data);
+    }
+    
+}
+
+// let subButton = document.getElementById("submitButton");
+
+// function subEvent(){
+    
+//     let link2 = document.getElementById("link2");
+//     link2.href = "index.html";
+// }
+
+// subButton.addEventListener("click" , subEvent);
